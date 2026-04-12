d6dd48fcecea636fe2fa48a21b8c50e2ba075eeb
mqjinwon <mqjinwon@gmail.com>
2026-04-12 22:48:27 +0900

    fix: guard heading update to avoid awk state bleed in migration

--- lib/ax-utils.sh
+++ lib/ax-utils.sh
@@ -99,7 +99,7 @@ ax_migrate_topic_file() {
 
     # 템플릿에서 해당 섹션 블록(헤딩 포함) 추출하여 append
     awk -v sec="$section" '
-      /^## / { heading=$0 }
+      /^## / { if (!in_s) heading=$0 }
       $0 == "<!-- BEGIN:" sec " -->" { in_s=1; if (heading) print heading; print; next }
       in_s && $0 == "<!-- END:" sec " -->" { print; in_s=0; heading=""; next }
       in_s { print }
